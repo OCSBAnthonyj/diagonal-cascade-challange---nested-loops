d44e3101e2831b9f4a1a412939cbd8484e81eb83
OCSBAnthonyj <113910453+OCSBAnthonyj@users.noreply.github.com>
2022-11-03 09:02:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 4; index++) {
-        index = 4 - index
-        for (let index = 0; index <= 4; index++) {
-        	
+    for (let index = 0; index <= 9; index++) {
+        x = index - 1
+        y = 0
+        for (let index2 = 0; index2 < index; index2++) {
+            led.toggle(x, y)
+            x += -1
+            y += 1
         }
+        basic.pause(100)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 4; index++) {
-        for (let index = 0; index <= 4; index++) {
-            led.toggle(0, index - 4)
-            basic.pause(100)
+    for (let index = 0; index <= 9; index++) {
+        x = 5 - index
+        y = 4
+        for (let index2 = 0; index2 < index; index2++) {
+            led.toggle(x, y)
+            x += 1
+            y += -1
         }
+        basic.pause(100)
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    control.reset()
 })
